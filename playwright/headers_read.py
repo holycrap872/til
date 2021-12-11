8493be42660948db08348a8e98709d2f419bcb17
Eric Rizzi <erizzi@amazon.com>
2021-12-11 11:22:35 -0500

    Give ability to read headers

---
+++ playwright/headers_read.py
@@ -0,0 +1,32 @@
+import asyncio
+from playwright.async_api import async_playwright
+from playwright.async_api._generated import Request, Response
+
+
+
+def log_request_headers(request: Request) -> None:
+    print("request", request.method, request.url)
+
+
+def log_response_headers(response: Response) -> None:
+    print("response", response.status, response.url)
+
+
+async def get_title(url: str) -> None:
+    async with async_playwright() as p:
+        print("hi")
+        browser = await p.chromium.launch(headless=False, slow_mo=5000)
+        page = await browser.new_page()
+        page.on("request", log_request_headers)
+        page.on("response", log_response_headers)
+        await page.goto(url)
+        title = await page.title()
+        await browser.close()
+        print(title)
+
+
+async def main():
+    task = asyncio.create_task(get_title("https://www.google.com"))
+    await task
+
+asyncio.run(main())
