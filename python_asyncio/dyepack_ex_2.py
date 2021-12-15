26dca53e5b6a5950df0d5116caabe4f4d4e582b5
Eric Rizzi <erizzi@amazon.com>
2021-12-15 09:22:19 -0500

    Good looking example

--- python_asyncio/dyepack_ex_2.py
+++ python_asyncio/dyepack_ex_2.py
@@ -23,37 +23,39 @@ class Job(typing.NamedTuple):
     orig_state: str
 
 
-async def do_job(job_str: str) -> None:
-    print(f"Waiting on {job_str}")
-    await asyncio.sleep(1)
+async def do_job(semaphore: asyncio.Semaphore, job_str: str) -> None:
+    async with semaphore:
+        print(f"Waiting on {job_str}")
+        await asyncio.sleep(1)
 
 
-async def do_prep(work_loop: AbstractEventLoop, job: Job) -> None:
+async def do_prep(work_loop: AbstractEventLoop, semaphore: asyncio.Semaphore, job: Job) -> None:
     for i in range(0, 4):
         new_job = Job(JobType._EXPLOIT, job.orig_state + f", prep_{i}")
-        await do_job(f"prep_{i}")
-        work_loop.create_task(do_exploit(work_loop, new_job))
+        await do_job(semaphore, f"prep_{i}")
+        work_loop.create_task(do_exploit(work_loop, semaphore, new_job))
 
 
-async def do_exploit(work_loop: AbstractEventLoop, job: Job) -> None:
+async def do_exploit(work_loop: AbstractEventLoop, semaphore: asyncio.Semaphore, job: Job) -> None:
     for i in range(0, 4):
         new_job = Job(JobType._VERIFY, job.orig_state + f", exploit_{i}")
-        await do_job(f"exploit_{i}")
-        work_loop.create_task(do_verify(work_loop, new_job))
+        await do_job(semaphore, f"exploit_{i}")
+        work_loop.create_task(do_verify(work_loop, semaphore, new_job))
 
 
-async def do_verify(work_loop: AbstractEventLoop, job: Job) -> None:
-    await do_job(job.orig_state + ", verify")
+async def do_verify(work_loop: AbstractEventLoop, semaphore: asyncio.Semaphore, job: Job) -> None:
+    await do_job(semaphore, job.orig_state + ", verify")
 
 
 async def main():
     loop = asyncio.get_event_loop()
+    semaphore = asyncio.Semaphore(10, loop=loop)
 
     current_tasks = asyncio.Task.all_tasks()
     assert len(current_tasks) == 1
     main_task = list(current_tasks)[0]
     
-    loop.create_task(do_prep(loop, Job(JobType._PREP, "start")))
+    loop.create_task(do_prep(loop, semaphore, Job(JobType._PREP, "start")))
 
     while True:
         pending = [task for task in asyncio.Task.all_tasks() if task != main_task and not task.done()]
