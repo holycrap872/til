fdc8c7e4b58fca68e58228752ad8e207732f654f
Eric Rizzi <erizzi@amazon.com>
2023-07-08 17:47:35 -0400

    Added a "this or that" grouping to argparse

--- python3/generic.py
+++ python3/generic.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import argparse
 
-def function(a, b, c):
-    print(a, b, c)
+def function(a, b, c, this, that):
+    print(a, b, c, this, that)
 
 if __name__ == "__main__":
 
@@ -13,6 +13,9 @@ if __name__ == "__main__":
                         default= None, type=str, required=False)
     parser.add_argument("-c", "--sea", help="option sea",
                         default=False, required=False, action='store_true')
+    this_or_that = parser.add_mutually_exclusive_group(required=True)
+    this_or_that.add_argument("--this", help="What dis", type=str)
+    this_or_that.add_argument("--that", help="What dat", type=str)
     args = parser.parse_args()
 
-    function(args.a, args.bee, args.sea)
+    function(args.a, args.bee, args.sea, args.this, args.that)
